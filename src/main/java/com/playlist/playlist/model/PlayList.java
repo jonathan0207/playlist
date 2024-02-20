4acc94d20b93c844dd2975e618fff983a49c2fd4
Jonathan Florez <joflorez@bancolombia.com.co>
2024-02-20 14:01:08 -0500

    create models

---
+++ src/main/java/com/playlist/playlist/model/PlayList.java
@@ -0,0 +1,48 @@
+package com.playlist.playlist.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="Songs")
+public class PlayList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    private String description;
+    private List<Song> songs;
+}
---
+++ src/main/java/com/playlist/playlist/model/Song.java
@@ -0,0 +1,67 @@
+package com.playlist.playlist.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name="Songs")
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String artist;
+    private String album;
+    private int year;
+    private String gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
